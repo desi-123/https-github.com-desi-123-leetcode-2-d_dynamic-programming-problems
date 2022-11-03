5b61e9b52e4528ae1ab0a72c010118016e460f71
Desalegn Wagaw <65276851+desi-123@users.noreply.github.com>
2022-11-02 17:41:23 -0700

    Update stone_game.py

--- stone_game.py
+++ stone_game.py
@@ -7,8 +7,8 @@ def stoneGame(piles):
         if (left, right) in dp:
             return dp[(left, right)]
         even = True if (right - left) % 2 else False
-        left = piles[left] if even else 0
-        right = piles[right] if even else 0
+        l = piles[left] if even else 0
+        r = piles[right] if even else 0
 
         dp[(left, right)] = max(dfs(left + 1, right) + left,
                                 dfs(left, right - 1) + right)
